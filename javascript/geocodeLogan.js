14e3e3e2af5ffa6cfaabb7bdb4a2b8a993e5bea4
lstroly <lstroly@gmail.com>
2018-01-21 14:03:27 -0500

    1.21 activity

--- javascript/geocodeLogan.js
+++ javascript/geocodeLogan.js
@@ -1,20 +1,56 @@
 $(document).ready(function(){
+  var city = "";
+  //geolocator starts here
   var key = "a9a61bb81ae8bbec";
-  navigator.geolocation.getCurrentPosition(success,error);
+  navigator.geolocation.getCurrentPosition(success, error);
   function error() {
-    alert("Sorry, we're unable to retrieve your location.");
+      alert("Sorry, we're unable to retrieve your location.");
   }
   function success(position) {
-    latitude = position.coords.latitude;
-    longitude = position.coords.longitude;
-    $.getJSON("https://api.wunderground.com/api/" + key + "/forecast/geolookup/conditions/q/" + latitude + "," + longitude + ".json", function(data) {
-      var city = data.location.city;
-      var state = data.location.state;
-      $(".city").html(city);
-      $(".state").html(state);
+      latitude = position.coords.latitude;
+      longitude = position.coords.longitude;
+      $.getJSON("https://api.wunderground.com/api/" + key + "/forecast/geolookup/conditions/q/" + latitude + "," + longitude + ".json", function(data) {
+          city = data.location.city;
+      });
+  };
+  //geolocator ends here
 
-      //TONY's CODE
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCWGmHUJjfT193FB4EjMqd49SAqb5AYwVs",
+    authDomain: "test-project-d958e.firebaseapp.com",
+    databaseURL: "https://test-project-d958e.firebaseio.com",
+    projectId: "test-project-d958e",
+    storageBucket: "test-project-d958e.appspot.com",
+    messagingSenderId: "496730470430"
+  };
 
-    });
-  }
+  firebase.initializeApp(config);
+  var uploader = document.getElementById('uploader');
+  var fileButton = document.getElementById('fileButton');
+  fileButton.addEventListener('change', function(e) {
+      var file = e.target.files[0];
+      var storageRef = firebase.storage().ref('img/' + file.name);
+      var metadata = {
+          customMetadata: {
+              'city': city
+          }
+      }
+      var task = storageRef.put(file, metadata);
+      task.on('state_changed', function progress(snapshot) {
+          var percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          uploader.value = percentage;
+          $("#well").append("<img src='#'>")  
+      }, function error(err) {
+      }, function complete() {
+      });
+  });
+
+  // Download starts here
+
+
+
+
+
+
 });
